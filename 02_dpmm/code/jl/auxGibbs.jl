6d8e17e26d2d47fe75b066e61d58a82a133e7863
luiarthur <luiarthur@gmail.com>
2015-11-06 18:43:11 -0800

    yes

--- 02_dpmm/code/jl/auxGibbs.jl
+++ 02_dpmm/code/jl/auxGibbs.jl
@@ -1,7 +1,20 @@
-using Distributions, DataFrames
+println("Loading Packages...")
+  using Distributions, DataFrames, UnicodePlots
+  #https://github.com/Evizero/UnicodePlots.jl
+println("Completed!")
 
-function table(x) 
-  dict = Dict(x[1] => 1)
+function sort_table(dict::Dict{Any,Any}, prop=false)
+  ord = sortperm( collect(values(dict)), rev=true )
+  n = length(dict)
+  sumv = sum(values(dict))
+  out = [ ( collect( keys(dict) )[o], collect( values(dict))[o] / ifelse(prop,sumv,1) ) for o in ord]
+
+  return out
+end
+
+function table(x)
+  dict = Dict()
+
   for u in x
     if haskey(dict, u)
       dict[u] += 1
@@ -9,28 +22,34 @@ function table(x)
       dict[u] = 1
     end
   end
+
   return dict
 end
 
-y = [rand(Normal(3,.1),30);
-     rand(Normal(10,.1),16); 
-     rand(Normal(20,.1),26)]
+s = 1
+#y = [rand(Normal(3 ,s),30);
+#     rand(Normal(1 ,s),3); 
+#     rand(Normal(20,s),15)]
+
+y = squeeze(readdlm("../../dat/hw2.dat")',1)
+histogram(y)
 #=
   F(theta) = N(theta,.1²)
   G₀ = N(0,1)
 =#
 
-n = size(y)[1]
-B = 1000
+n = length(y)
+B = 10000
+burn = Int64(B * .3)
 t = ones(B,n)
 a = 1
 p = 0
-G0(;n=1) = rand(Normal(0,1),n)
-f(x, theta) = pdf( Normal(theta,1), x )
-lf(x, theta) = logpdf( Normal(theta,1), x )
-lp(x) = logpdf( Normal(0,3), x )
+G0 = rand(Normal(0,1),B)
+f(x, theta) =     pdf( Normal(theta,s), x )
+lf(x, theta) = logpdf( Normal(theta,s), x )
+lg0(x) = logpdf( Normal(0,1), x )
 acc_t = 0
-cs = 2
+cs = 10
 
 for b in 2:B
   t[b,:] = t[b-1,:]
@@ -39,20 +58,21 @@ for b in 2:B
   for i in 1:n
     tb = t[b,:]
     ti = tb[i]
-    ut_wo_ti = unique( tb[setdiff(1:n,i)] )
+    #ut_wo_ti = unique( tb[find(x-> x != i, 1:n)] )
+    ut_wo_ti = unique( tb[setdiff(1:n, i)] )
     k = length( ut_wo_ti )
 
     if in(ti, ut_wo_ti) # if tᵢ is not a singleton
-      p = G0(1)[1]
+      p = G0[b]
     end
 
-    probs = fill(a * f(y[i], p) / (n-1+a), k+1)
+    probs = fill(a * f(y[i], p) , k+1)
     for j in 1:k
       utj = ut_wo_ti[j]
-      probs[j] = sum(utj .== tb) * f(y[i], utj) / (n-1+a)
+      probs[j] = sum(utj .== tb) * f(y[i], utj)
     end
 
-    t[b,i] = wsample([ut_wo_ti; p], probs)
+    t[b,i] = wsample( [ut_wo_ti; p] , probs )
   end # for i in 1:n
   
   # Update θ | y
@@ -62,21 +82,25 @@ for b in 2:B
     u = u[1]
     ind = find(x -> x == u, tb)
     cand = rand(Normal(u,cs))
-    lg1 = sum( [lf(i,cand) for i in ind] ) + lp(cand)
-    lg2 = sum( [lf(i,u) for i in ind] ) + lp(u)
+    lg1 = sum( [lf(y[i],cand) for i in ind] ) + lg0(cand)
+    lg2 = sum( [lf(y[i],u) for i in ind] ) + lg0(u)
     lg = lg1 - lg2
 
     if lg > log(rand())
       #acc_t += 1
-      t[b,i] = cand
+      t[b,ind] = cand
     end
   end
 
-  if b%(B/10)==0 print("\r",round(100*b/B),"%") end
+  if b%(B/20)==0 print("\r",round(100*b/B),"%") end
+  #print("\r",b)
 end # for b in 1:B
 
 #=
   include("auxGibbs.jl")
-  tab = table(t[2,:])
+  tab = table(t[B,:])
   mt = [mean(t[:,j]) for j in 1:n]
+  mns = [length(unique(t[b,:])) for b in (burn+1):B]
+  tabm = table(mns)
+  sort_table(tabm,true)
 =#
