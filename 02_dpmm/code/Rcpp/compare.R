3785bd8168b9948e7450415d8a6c108793bcd013
luiarthur <luiarthur@gmail.com>
2015-11-07 23:54:22 -0800

    yes

--- 02_dpmm/code/Rcpp/compare.R
+++ 02_dpmm/code/Rcpp/compare.R
@@ -4,13 +4,13 @@ sourceCpp("dp_arma.cpp")
 source("auxGibbs.R")
 y <- read.table("../../dat/hw2.dat")[[1]]
 
-B <- 5000
-burn <- B * .3
+B <- 10000
+burn <- round(B * .3)
 
 # Rcpp is 10 times faster than R
-microbenchmark(outCpp <- auxGibbsCpp(y,a=2,s=1,cs=3,B=B),
-                 outR <-   auxGibbsR(y,a=2,s=1,cs=3,B=B), 
-                 times=1, control=list("order"="inorder"))
+mb <- microbenchmark(outCpp <- auxGibbsCpp(y,a=2,s=1,cs=3,B=B),
+                       outR <-   auxGibbsR(y,a=2,s=1,cs=3,B=B), 
+                       times=1, control=list("order"="inorder"))
 
 out <- list(outCpp, outR)
 
@@ -23,9 +23,9 @@ for ( i in 1:length(out) ) {
 }
 par(mfrow=c(1,1))
 
-plot(density(apply(out[[1]],2,mean)),  ylim=c(0,.4), col="purple", lwd=2)
-lines(density(apply(out[[2]],2,mean)), ylim=c(0,.4), col="red", lwd=2)
+ plot(density(apply(tail(out[[1]],B-burn),2,mean)), ylim=c(0,.4), lwd=2, col="purple")
+lines(density(apply(tail(out[[2]],B-burn),2,mean)), ylim=c(0,.4), lwd=2, col="red")
 
-plot(density(apply(out[[1]],2,sd)),  ylim=c(0,3), col="purple", lwd=2)
-lines(density(apply(out[[2]],2,sd)), ylim=c(0,3), col="red", lwd=2)
+ plot(density(apply(tail(out[[1]],B-burn),2,sd)), ylim=c(0,3), lwd=2, col="purple")
+lines(density(apply(tail(out[[2]],B-burn),2,sd)), ylim=c(0,3), lwd=2, col="red")
 
