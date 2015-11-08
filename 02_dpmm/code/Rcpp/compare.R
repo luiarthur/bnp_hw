5639f247bbe244bc08bd0908f392a8d774db9819
luiarthur <luiarthur@gmail.com>
2015-11-07 22:59:37 -0800

    yes

--- 02_dpmm/code/Rcpp/compare.R
+++ 02_dpmm/code/Rcpp/compare.R
@@ -1,3 +1,4 @@
+library(microbenchmark)
 library(Rcpp)
 sourceCpp("dp_arma.cpp")
 source("auxGibbs.R")
@@ -6,14 +7,11 @@ y <- read.table("../../dat/hw2.dat")[[1]]
 B <- 10000
 burn <- B * .3
 
+# Rcpp is 30 times faster than julia
+microbenchmark(outCpp <- auxGibbsCpp(y,a=2,s=1,cs=3,B=B),
+               outR <- auxGibbsR(y,a=2,s=1,cs=3,B=B))
 
-system.time(out <- gibbs(y,a=2,s=1,cs=3,B=10000)) # 30 times faster than julia
-
-tab <- table(out[B,])
-mt <- apply(tail(out,B-burn),2,mean)
-mns <- apply(tail(out,B-burn),1,function(x) length(unique(x)))
-tabm <- table(mns) / sum(table(mns))
-plot(tabm)
-
+out <- list(outCpp, outR)[[1]] # change this
+mean.theta <- apply(tail(out,B-burn),2,mean)
 plot(y,pch=20,col='grey',cex=2)
-points(mt,pch=20,col=rgb(0,0,1,.5))
+points(mean.theta,pch=20,col=rgb(0,0,1,.5))
