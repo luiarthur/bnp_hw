448e847db464908b68147b4264ebf9473fef3a89
Arthur Lui <luiarthur@gmail.com>
2015-11-18 11:30:55 -0800

    yes

--- 02_dpmm/code/Rcpp/dppois.R
+++ 02_dpmm/code/Rcpp/dppois.R
@@ -6,11 +6,12 @@ n <- nrow(dat)
 #plot(x,y)
 
 Sys.setenv("PKG_CXXFLAGS"="-std=c++11") # enable c++11
-system.time( sourceCpp("dppois.cpp") )
+system.time( sourceCpp("dppois.cpp", showOutput=T) )
 system.time(z <- dppois(y,x,1,1,1,1,1,1,1,1,100,.0001,B=10000))
 
 plot(tail(z$alpha,2000),type='l')
 plot(tail(z$mu,2000),type='l')
 plot(tail(z$tau,2000),type='l')
 plot(tail(z$beta,2000),type='l')
+
 
