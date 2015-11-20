3e77adfc1829e47aca316636d50b7ce900fc14ba
luiarthur <luiarthur@gmail.com>
2015-11-19 23:57:36 -0800

    yes

--- 02_dpmm/code/Rcpp/dppois.R
+++ 02_dpmm/code/Rcpp/dppois.R
@@ -5,25 +5,44 @@
 library(Rcpp)
 source("../../../R_Functions/plotPost.R",chdir=T)
 dat <- read.table("../../dat/fabric.dat",header=T)
+cs <- function(x) ( x - mean(x) ) / sd(x)
 x <- dat$length
 y <- dat$faults
 n <- nrow(dat)
 #plot(x,y)
+Sys.setenv("PKG_CXXFLAGS"="-std=c++11") # enable c++11, for RcppArmadillo
 
-Sys.setenv("PKG_CXXFLAGS"="-std=c++11") # enable c++11
+# DP ###
 system.time( sourceCpp("dppois.cpp") )#, showOutput=T) )
-system.time(z <- dppois(y,x,a_mu=1,b_mu=1,a_tau=1,b_tau=1,
+system.time(z <- dppois(y,cs(x),a_mu=1,b_mu=1,a_tau=1,b_tau=1,
                         a_alpha=1,b_alpha=1,m_beta=1,s_beta=1,
-                        cs_mu=1,cs_beta=.0003,B=100000))
-
-par(mfrow=c(4,1))
-plot.post(tail(z$alpha,2000),main=bquote(alpha),ylab='')
-plot.post(tail(z$mu,2000),main=bquote(mu),ylab='')
-plot.post(tail(z$tau,2000),main=bquote(tau),ylab='')
-plot.post(tail(z$beta,2000),main=bquote(beta),ylab='')
-par(mfrow=c(4,1))
-
+                        cs_mu=1,cs_beta=.2,B=100000))
 z$acc_beta
 z$acc_mu
 
+par(mfrow=c(4,1))
+  plot(tail(z$alpha,2000),main=bquote(alpha),ylab='',type='l')
+  plot(tail(z$mu,2000),main=bquote(mu),ylab='',type='l')
+  plot(tail(z$tau,2000),main=bquote(tau),ylab='',type='l')
+  plot(tail(z$beta,2000),main=bquote(beta),ylab='',type='l')
+par(mfrow=c(1,1))
+
+ut <-  apply(tail(z$theta,1000),1,function(x) length( unique(x) ))
+tut <- table(ut)
+plot(tut / sum(tut))
+
+plot(tail(z$theta[,1],2000),cex=.1,col=rgb(.1,.1,.1,.1))
+for ( k in 1:ncol(z$theta) )
+  points(tail(z$theta[,k],2000),cex=.1,col=rgb(.1,.1,.1,.1))
+
+# Simple Poisson Regression ######
+system.time( sourceCpp("regpois.cpp") )
+system.time(rgp <- regpois(y,cs(x),a=1,b=1,m=0,s2=.1,cs_beta=.2,B=1000000))
+rgp$acc_beta
+par(mfrow=c(2,1))
+plot(tail(rgp$theta,2000),type='l'); mean(tail(rgp$theta,2000))
+plot(tail(rgp$beta,2000),type='l'); mean(tail(rgp$beta,2000))
+par(mfrow=c(1,1))
+
+plot(tail(rgp$beta,5000), tail(rgp$theta,5000), col=rgb(.5,.5,.5,.1:5000/5000),pch=20,cex=.1)
 
---
+++ 02_dpmm/code/Rcpp/regpois.cpp
@@ -0,0 +1,54 @@
+//[[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+#include <iostream>
+
+using namespace std;
+using namespace arma;
+using namespace Rcpp;
+
+double update_theta(double beta, double sum_y, vec x, double a, double b) {
+  return rgamma( 1, a + sum_y, 1 / ( b + sum(exp(beta*x)) ) )[0];
+}
+
+double update_beta(double beta, double theta, double sum_xy, vec x, double m, double s2, 
+    double cs, int* acc) {
+  double lg1, lg2, lg, cand, beta_new;
+
+  beta_new = beta;
+  cand = rnorm(1,beta,cs)[0];
+
+  lg1 = -pow(cand-m,2) / (2*s2) + cand * sum_xy - theta * sum( exp(cand*x) );
+  lg2 = -pow(beta-m,2) / (2*s2) + beta * sum_xy - theta * sum( exp(beta*x) );
+  lg = lg1 - lg2;
+
+  if (lg > log(randu()) ) {
+    beta_new = cand;
+    *acc = *acc + 1;
+  }
+
+  return beta_new;
+}
+
+
+//[[Rcpp::export]]
+List regpois(vec y, vec x, double a_beta, double b_beta, double m, double s2, double cs_beta, int B) {
+  vec theta = ones<vec>(B);
+  vec beta = zeros<vec>(B);
+  double sum_y = sum(y);
+  vec temp_vec = y.t() * x;
+  double sum_xy = temp_vec[0];
+  int acc_beta = 0;
+  List ret;
+
+  for (int b=1; b<B; b++) {
+    theta[b] = update_theta(beta[b-1], sum_y, x, a_beta, b_beta);
+    beta[b] = update_beta(beta[b-1], theta[b], sum_xy, x, m, s2, cs_beta, &acc_beta);
+    cout << "\r Progress: " << b*100/B << "%";
+  }
+
+  ret["theta"] = theta;
+  ret["beta"] = beta;
+  ret["acc_beta"] = acc_beta*1.0 / B;
+
+  return ret;
+}
