995990af17f310e464b94c991e4ba1badb6a197a
Arthur Lui <luiarthur@gmail.com>
2015-11-30 13:47:32 -0800

    yes

--- project/bnp_spatialDP/code/sbnp.R
+++ project/bnp_spatialDP/code/sbnp.R
@@ -11,29 +11,25 @@ mo  <- as.numeric(substr(rain$DATE,5,6))
 stations <- as.character(rain$STATION)
 ustation <- unique(stations)
 
-plot(mo[yr==2014], rain$TPCP[yr==2014], type='p')
-
-par(mfrow=c(4,4))
-for (yy in sort(unique(yr))) {
-  plot(mo[yr==yy], rain$TPCP[yr==yy], type='p', main=yy)
-}
-
-
-stationView <- function(stat) {
-  s.yr  <- as.numeric(substr(stat$DATE,1,4))
+stationView <- function(stat, s.yr=0) {
+  if(s.yr==0) par(mfrow=c(4,4))
+  if (s.yr == 0) s.yr  <- as.numeric(substr(stat$DATE,1,4))
   s.mo  <- as.numeric(substr(stat$DATE,5,6))
 
-  par(mfrow=c(4,4))
+
   for (yy in sort(unique(s.yr))) {
     stat.v <- stat$TPCP[s.yr==yy]
     stat.mn <- min(stat.v)
     stat.mx <- max(stat.v)
     #plot(s.mo[s.yr==yy], stat.v, type='p', main=yy)
-    plot(s.mo[s.yr==yy], (stat.v-stat.mn)/(stat.mx-stat.mn), type='p', main=yy, xlim=c(1,12))
+    plot(s.mo[s.yr==yy], (stat.v-stat.mn)/(stat.mx-stat.mn), type='p', main=paste(stat$STATION_NAME[1],"\n",yy), xlim=c(1,12))
   }
+  par(mfrow=c(1,1))
 }
 
 stationView(rain[which(stations==ustation[29]),]) # 6, 16, 26, 29
 stationView(rain[which(stations==ustation[16]),]) # 6, 16, 26, 29
 stationView(rain[which(stations==ustation[26]),]) # 6, 16, 26, 29
 stationView(rain[which(stations==ustation[6]),]) # 6, 16, 26, 29
+
+# n=4, T=16
