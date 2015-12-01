e2db76fd313b70b1ee3b2c314d982a3459282c88
luiarthur <luiarthur@gmail.com>
2015-12-01 13:44:21 -0800

    yes

---
+++ project/bnp_spatialDP/data/retrieve/grid.R
@@ -0,0 +1,27 @@
+# https://eosweb.larc.nasa.gov/cgi-bin/sse/daily.cgi?&ye=2004&lat=4133&submit=Submit&me=12&sitelev=&email=&step=1&p=swv_dwn&de=31&ms=1&ys=2004&plot=swv_dwn&lon=114124&ds=1
+# Generate Grid Points of Interest:
+library(LatticeKrig) # quilt.plot
+library(maps) # map()
+
+gd.locs <- 70
+lon.seq <- seq(-122,-120,len=gd.locs)
+lat.seq <- seq(37,35,len=gd.locs)
+grid <- matrix(0,gd.locs^2,3)
+colnames(grid) <- c("lon","lat","value")
+for (i in 1:gd.locs) {
+  for (j in 1:gd.locs) {
+    grid[(i-1)*gd.locs + j,1:2] <- c(lon.seq[i],lat.seq[j])
+  }
+}
+grid[,3] <- rnorm(gd.locs^2,1:2)
+grid <- grid[which(-84.5 - grid[,1]>grid[,2]),]
+grid <- grid[which(-85.5 - grid[,1]<grid[,2]),]
+
+quilt.plot(grid[,1],grid[,2],grid[,3], #lon,lat,val
+           xlim=c(-122.2,-119.8), ylim=c(34.8,37.2),bty='n',fg='grey',
+           col= colorRampPalette(c('dark red','white','dark blue'))(100))
+map('county',add=T,lwd=2,col='grey40')
+
+write.csv(round(grid[,1:2],2),"gridlocs.dat",quote=F,row.names=F)
+###
+
--- project/bnp_spatialDP/data/retrieve/retrieve.py
+++ project/bnp_spatialDP/data/retrieve/retrieve.py
@@ -4,20 +4,32 @@ import urllib
 import urllib2
 import string
 import re # Regexpr
+import numpy as np
+import sys
 
-values = {'lat':'34', 'lon':'-121', 'ms':'1', 'ds':'1', 'ys':'1985', 'me':'12', 'de':'31', 'ye':'2004', 'submit':'Submit'}
+gridlocs = np.genfromtxt("gridlocs.dat",delimiter=',',skiprows=1)
+n = len(gridlocs)
+
+for i in range(0,n):
+lat = gridlocs[i,1]
+lon = gridlocs[i,0]
+values = {'lat':lat, 'lon':lon, 'ms':'1', 'ds':'1', 'ys':'1985', 'me':'12', 'de':'31', 'ye':'2004', 'submit':'Submit'}
 data = urllib.urlencode(values)
 req = urllib2.Request(site,data)
-
+#
 opener1 = urllib2.build_opener()
 page1=opener1.open(req)
-
+#
 htmlfile=page1.read()
-htmlfile
 txt = re.search("<a href=.*Download a text file",htmlfile)
 txtpath = "https://eosweb.larc.nasa.gov" + txt.group(0).split('"')[1]
 txtfile = urllib2.urlopen(txtpath)
 out = txtfile.read()
-fout = open('34-121.dat', "wb")
-fout.write(out)
+out2 = out.split('\n')
+out2 = out2[8:len(out2)-1]
+fout = open('temps/'+`i`+'.dat', "wb")
+fout.write('\n'.join(out2))
 fout.close()
+#print "\rProgress: %d%s" %(round(i*100/n),"%"),
+print "\rProgress: %d%s%d" %(i,"/",n),
+sys.stdout.flush()
