10bfaec19631b68ef5b1f34087c37c970306dca5
luiarthur <luiarthur@gmail.com>
2015-12-01 23:09:09 -0800

    yes

--- project/bnp_spatialDP/code/sdp.R
+++ project/bnp_spatialDP/code/sdp.R
@@ -1,7 +1,8 @@
 library(Rcpp)
 Sys.setenv("PKG_CXXFLAGS"="-std=c++11") # enable c++11, for RcppArmadillo
 source("../../../R_Functions/plotPost.R",chdir=T)
-load("../data//Y.RData")
+read.csv("../data/predlocs.dat")
+load("../data/Y.RData")
 
 Ydim <- dim(Yout)
 locs <- Ydim[1]
