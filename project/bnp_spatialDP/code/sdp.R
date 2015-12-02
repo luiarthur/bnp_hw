9f999fe44d048bfe86b054f08973a0ee1ab59d51
luiarthur <luiarthur@gmail.com>
2015-12-01 17:24:42 -0800

    yes

---
+++ project/bnp_spatialDP/code/sdp.R
@@ -0,0 +1,6 @@
+library(Rcpp)
+Sys.setenv("PKG_CXXFLAGS"="-std=c++11") # enable c++11, for RcppArmadillo
+source("../../../R_Functions/plotPost.R",chdir=T)
+load("../data//Y.RData")
+Y <- Yout; rm(Yout)
+
