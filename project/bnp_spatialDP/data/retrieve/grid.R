90be4762d26e96bf8cf080ed93c3c73e46e9aea9
luiarthur <luiarthur@gmail.com>
2015-12-01 16:16:45 -0800

    yes

--- project/bnp_spatialDP/data/retrieve/grid.R
+++ project/bnp_spatialDP/data/retrieve/grid.R
@@ -3,9 +3,17 @@
 library(LatticeKrig) # quilt.plot
 library(maps) # map()
 
-gd.locs <- 70
-lon.seq <- seq(-122,-120,len=gd.locs)
-lat.seq <- seq(37,35,len=gd.locs)
+gd.locs <- 10
+
+lon.up <- -114#-120
+lon.lo <- -126#-122
+lat.up <- 44#37
+lat.lo <- 32#35
+lon.seq <- seq(lon.lo,lon.up,len=gd.locs)
+lat.seq <- seq(lat.lo,lat.up,len=gd.locs)
+int.up <- -79.5
+int.lo <- -85.5
+
 grid <- matrix(0,gd.locs^2,3)
 colnames(grid) <- c("lon","lat","value")
 for (i in 1:gd.locs) {
@@ -14,13 +22,15 @@ for (i in 1:gd.locs) {
   }
 }
 grid[,3] <- rnorm(gd.locs^2,1:2)
-grid <- grid[which(-84.5 - grid[,1]>grid[,2]),]
-grid <- grid[which(-85.5 - grid[,1]<grid[,2]),]
-print(nrow(grid))
+grid <- grid[which(int.up - grid[,1]>grid[,2]),]
+grid <- grid[which(int.lo - grid[,1]<grid[,2]),]
 
 quilt.plot(grid[,1],grid[,2],grid[,3], #lon,lat,val
-           xlim=c(-122.2,-119.8), ylim=c(34.8,37.2),bty='n',fg='grey',
+           #xlim=c(lon.lo-1,lon.up-1), ylim=c(lat.lo-1,lat.up-1),
+           bty='n', fg='grey',
            col= colorRampPalette(c('dark red','white','dark blue'))(100))
-map('county',add=T,lwd=2,col='grey40')
+map('county',add=T,lwd=2,col='pink')
+map('state',add=T,lwd=2,col='grey40')
 
 write.csv(round(grid[,1:2],2),"gridlocs.dat",quote=F,row.names=F)
+print(nrow(grid))
--- project/bnp_spatialDP/data/retrieve/reduce.R
+++ project/bnp_spatialDP/data/retrieve/reduce.R
@@ -1,10 +1,11 @@
+### Reduce Data
 latlon <- read.csv("gridlocs.dat")
 files <- list.files("temps")
 filenum <- as.numeric( sub(".dat",files,repl="") )
 L <- length(files)
 ord <- order(filenum)
 i <- 0
-test <- read.table(paste0('temps/',f),skip=8,header=T) 
+test <- read.table('temps/0.dat',skip=8,header=T) 
 n <- nrow(test)
 
 # Assuming Years are 
@@ -32,9 +33,34 @@ for (f in files[ord]) {
       fbig[j,3] <- lat
       fbig[j,4] <- lon
       ind <- as.logical( (dat$YEAR == yr) * (dat$MO == mo) )
-      fbig[j,5] <- mean(dat$swv_dwn[which(ind)])
+      fbig[j,5] <- mean(dat$T10M[which(ind)])
     }
   }
   Y[[i]] <- fbig
   cat("\rProgress: ", i, "/",L)
 }
+Y[[1]]
+
+### Visualize
+library(LatticeKrig) # quilt.plot
+library(maps) # map()
+
+view <- function(y,mo,yr) {
+  M <- matrix(0,L,ncol(y[[1]]))
+  colnames(M) <- colnames(y[[1]])
+  i <- 0
+  for (yy in y) {
+    i <- i + 1
+    ind <- as.logical((yy[,"year"]==yr) * (yy[,"mo"]==mo))
+    M[i,] <- yy[ind,]
+  }
+  quilt.plot(M[,"lon"],M[,"lat"],M[,"C"], #lon,lat,val
+             xlim=c(-122.2,-119.8), ylim=c(34.8,37.2),bty='n',fg='grey',
+             col= colorRampPalette(c('dark red','white','dark blue'))(100))
+  map('county',add=T,lwd=2,col='grey40')
+}
+
+par(mfrow=c(3,1))
+view(Y,1,1985)
+view(Y,1,1994)
+view(Y,1,2004)
