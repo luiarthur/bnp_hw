201408b339455b2d00527134eaaa99344691d945
Arthur Lui <luiarthur@gmail.com>
2015-12-31 17:50:30 -0800

    yes

--- logistic/dp_logistic.R
+++ logistic/dp_logistic.R
@@ -1,5 +1,8 @@
-# (z_i, x_i1,...,x_ip) ~ N_{p+1}(mu,Sig)
-#               mu,Sig ~ G
-#                    G ~ DP(a, G0), G0 = N(m,S)
-#                    m ~ 
-#                    S ~
+#  mu = (mu_z, mu_x)
+#   S = S_{zz}, S_{zx}
+#       S_{xz}, S_{xx}
+
+# [y_i , x_i1, x_ik] ~ Bern(y_i, p_i) x N_k(mu_x, S_{xx}), 
+#   for S_{zx} = 0, 
+#   p_i = mu_z / sqrt(S_zz), where S_zz is a constant 
+# mu_x 
